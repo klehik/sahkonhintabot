f25c8bece2e577384dbb240698f83b010289f304
AA4085 <aa4085@student.jamk.fi>
2023-05-03 11:03:54 +0300

    reorganizing

--- main.py
+++ main.py
@@ -12,63 +12,58 @@ from log import init_logger
 import logging
 
 
-
-
-def check_if_last_day_of_month():
-    now = datetime.now()
-    is_last_day_of_month = True if now.day == get_days_in_month(now) else False
-
-    if is_last_day_of_month:
-       
-        tweet_monthly_report(now)
-   
-
 def tweet_reports():
-    
-    is_hot = True if os.getenv("HOT") == 'True' else False
+
+    is_hot = True if os.getenv("HOT") == "True" else False
 
     now = datetime.now()
     one_day = timedelta(days=1)
-    tomorrow = now+one_day
+    tomorrow = now + one_day
     report = generate_day_ahead_report(tomorrow)
 
-   
-    
-
     if not report.dataframe.empty:
-
         # day ahead
-        graph_settings = {"bar_labels": True, "label_rotation": 0, "bars_from_start": 5, "bar_label_font_size": 8}
+        graph_settings = {
+            "bar_labels": True,
+            "label_rotation": 0,
+            "bars_from_start": 5,
+            "bar_label_font_size": 8,
+        }
         report.plot_bar_graph(graph_settings)
-        message_day = compile_day_ahead_message(report, 'below_average')
+        message_day = compile_day_ahead_message(report, "below_average")
         logging.info("Day-ahead message: \n{}".format(message_day))
 
-         # 7 day
+        # 7 day
         report_7 = generate_7_day_report(tomorrow)
         graph_settings_7 = {"hourly": True, "label_rotation": 0, "bars_from_start": 32}
         report_7.plot_bar_graph(graph_settings_7)
         message_7 = compile_7_day_message(report_7)
         logging.info("7-day message: {}".format(message_7))
-    
+
         # 28 day
         report_28 = generate_28_day_report(tomorrow)
-        graph_settings_28 = {"hourly": False, "label_rotation": 45, "bars_from_start": 6, "bar_label_font_size": 6}
+        graph_settings_28 = {
+            "hourly": False,
+            "label_rotation": 45,
+            "bars_from_start": 6,
+            "bar_label_font_size": 6,
+        }
         report_28.plot_bar_graph(graph_settings_28)
         message_28 = compile_28_day_message(report_28)
         logging.info("28-day message: {}".format(message_28))
 
         # tweet as thread
         if is_hot:
-            
+
             # tweet day ahead
             tweet = tweet_with_image(report.bar_graph_path, message_day)
-            
+
             # reply to day ahead tweet
             tweet_7 = reply_to_tweet(message_7, report_7.bar_graph_path, tweet.id)
 
             # reply to 7 day tweet
             tweet_28 = reply_to_tweet(message_28, report_28.bar_graph_path, tweet_7.id)
-            
+
             # save first tweet id
             database.add_tweet(report, tweet.id)
 
@@ -82,57 +77,64 @@ def tweet_reports():
 
 def tweet_monthly_report(now):
 
-    
-    is_hot = True if os.getenv("HOT") == 'True' else False
-    one_month = timedelta(days=35) # to get previous month number
+    is_hot = True if os.getenv("HOT") == "True" else False
+    one_month = timedelta(days=35)  # to get previous month number
 
     # current month
     report_current = generate_month_report(now)
 
     # previous month
-    report_previous = generate_month_report(now-one_month)
+    report_previous = generate_month_report(now - one_month)
 
-    graph_settings = {"hourly": False, "label_rotation": 45, "bars_from_start": 6, "bar_label_font_size": 6}
+    graph_settings = {
+        "hourly": False,
+        "label_rotation": 45,
+        "bars_from_start": 6,
+        "bar_label_font_size": 6,
+    }
     report_current.plot_bar_graph(graph_settings)
     message = compile_monthly_message(report_current, report_previous)
     print(message)
 
     if is_hot:
         tweet_with_image(report_current.bar_graph_path, message)
-        
+
     else:
         logging.info("The bot is not hot")
 
 
-    
 def retweet_day_report():
     now = datetime.now()
     date = f"{str(now.day)}.{str(now.month)}.{str(now.year)}"
     tweet = database.get_tweet(date)
     if tweet:
-        id = tweet['tweet_id']
+        id = tweet["tweet_id"]
         retweet(id)
-    else: 
-        logging.info("Tweet id on {} not found from database".format(date))   
+    else:
+        logging.info("Tweet id on {} not found from database".format(date))
 
 
-    
+def check_if_last_day_of_month():
+    now = datetime.now()
+    is_last_day_of_month = True if now.day == get_days_in_month(now) else False
+
+    if is_last_day_of_month:
+        tweet_monthly_report(now)
+
 
 if __name__ == "__main__":
     load_dotenv(".env")
-    
+
     init_logger()
     logging.info("Setting up schedule, bot is hot: {}".format(os.getenv("HOT")))
 
-    schedule.every().day.at("14:01").do(tweet_reports)
+    schedule.every().day.at("14:10").do(tweet_reports)
     schedule.every().day.at("07:00").do(retweet_day_report)
     schedule.every().day.at("11:00").do(check_if_last_day_of_month)
-          
+
     logging.info("Jobs scheduled: {}".format(schedule.get_jobs()))
-    
-    
+
     while True:
-        
+
         schedule.run_pending()
         time.sleep(1)
-        
--- message.py
+++ message.py
@@ -1,4 +1,11 @@
-from utils import format_difference, format_percentage, format_price, get_month_name, format_difference, round_half_up
+from utils import (
+    format_difference,
+    format_percentage,
+    format_price,
+    get_month_name,
+    format_difference,
+    round_half_up,
+)
 from datetime import datetime
 import database
 import os
@@ -9,18 +16,21 @@ def compile_below_average_periods_message(report):
 
     below_average_periods = report.calculate_below_average_periods()
 
-    range_str = ''
+    range_str = ""
     if len(below_average_periods) > 1:
         range_str = "Ajanjaksot, joissa"
     else:
         range_str = "Ajanjakso, jossa"
 
-    below_average_message = f"{range_str} hinta pysyttelee alle vuorokauden keskiarvon\n"
-    if len(below_average_periods) <=3:
+    below_average_message = (
+        f"{range_str} hinta pysyttelee alle vuorokauden keskiarvon\n"
+    )
+    if len(below_average_periods) <= 3:
         for r in below_average_periods:
-            mean = str(r[2]).replace('.',',')
-            below_average_message+= f"{str(r[0])}.00 - {str(r[1])}.00, keskihinta {mean}\n"
-
+            mean = str(r[2]).replace(".", ",")
+            below_average_message += (
+                f"{str(r[0])}.00 - {str(r[1])}.00, keskihinta {mean}\n"
+            )
 
     return below_average_message
 
@@ -28,40 +38,37 @@ def compile_below_average_periods_message(report):
 def compile_average_periods_message(report):
 
     df = report.dataframe
-    
+
     """ period08 =  df['price'].iloc[0:8]
     period816 =  df['price'].iloc[7:16]
     period1624 =  df['price'].iloc[15:24] """
 
-    df_split = np.array_split(df['price_rounded'], 3)
+    df_split = np.array_split(df["price_rounded"], 3)
 
-    period08 =  df_split[0]
-    period816 =  df_split[1]
-    period1624 =  df_split[2]
-    
-    period08_mean =  format_price(round_half_up(period08.mean(), 2))
-    period816_mean =  format_price(round_half_up(period816.mean(), 2))
-    period1624_mean =  format_price(round_half_up(period1624.mean(), 2))
+    period08 = df_split[0]
+    period816 = df_split[1]
+    period1624 = df_split[2]
 
-    message = 'Keskihinnat aikaväleillä\n'
-    message += f'0.00 - 8.00, {period08_mean}\n'
-    message += f'8.00 - 16.00, {period816_mean}\n'
-    message += f'16.00 - 24.00, {period1624_mean}\n'
-    
-    
+    period08_mean = format_price(round_half_up(period08.mean(), 2))
+    period816_mean = format_price(round_half_up(period816.mean(), 2))
+    period1624_mean = format_price(round_half_up(period1624.mean(), 2))
+
+    message = "Keskihinnat aikaväleillä\n"
+    message += f"0.00 - 8.00, {period08_mean}\n"
+    message += f"8.00 - 16.00, {period816_mean}\n"
+    message += f"16.00 - 24.00, {period1624_mean}\n"
 
     return message
 
 
-
 def compile_day_ahead_message(report, type):
     insights = report.insights
-    #num_of_periods = len(below_average_periods)
+    # num_of_periods = len(below_average_periods)
     hashtags = "#sähkönhinta"
-    
-    mean = format_price(insights['mean'])
-    min = format_price(insights['min'])
-    max = format_price(insights['max'])
+
+    mean = format_price(insights["mean"])
+    min = format_price(insights["min"])
+    max = format_price(insights["max"])
 
     tax = os.getenv("TAX")
 
@@ -69,93 +76,95 @@ def compile_day_ahead_message(report, type):
     message += f"Alin: {min}\n"
     message += f"Ylin: {max}\n"
     message += f"Keskihinta: {mean}\n\n"
-    
-    if type == 'below_average_periods':
+
+    if type == "below_average_periods":
         seconary_message = compile_below_average_periods_message(report)
     else:
         seconary_message = compile_average_periods_message(report)
-    
+
     if (len(message) + len(seconary_message)) < 280:
-            message += seconary_message
+        message += seconary_message
 
     if (len(message) + len(hashtags)) < 280:
         message += f"\n{hashtags}"
     print(len(message))
     return message
 
+
 def compile_today_reply_message():
-    
+
     message = "Tuntihinnat tänään"
     return message
 
+
 def compile_28_day_message(report):
     insights = report.insights
-    mean = format_price(insights['mean'])
-    min = format_price(insights['min'])
-    max = format_price(insights['max'])
+    mean = format_price(insights["mean"])
+    min = format_price(insights["min"])
+    max = format_price(insights["max"])
 
     message = f"28 vuorokauden keskihinta: {mean} snt/kWh"
     return message
 
-    
+
 def compile_7_day_message(report):
     insights = report.insights
-    mean = format_price(insights['mean'])
+    mean = format_price(insights["mean"])
     message = f"7 vuorokauden keskihinta: {mean} snt/kWh"
     return message
+
 
 def compile_monthly_message(report_current, report_previous):
 
     insights_curr = report_current.insights
     insights_prev = report_previous.insights
 
-    diff = insights_curr['mean'] - insights_prev['mean']
-    diff_percent = diff / insights_prev['mean'] * 100
-    
-    min = format_price(insights_curr['min'])
-    max = format_price(insights_curr['max'])
-    mean = format_price(insights_curr['mean'])
+    diff = insights_curr["mean"] - insights_prev["mean"]
+    diff_percent = diff / insights_prev["mean"] * 100
+
+    min = format_price(insights_curr["min"])
+    max = format_price(insights_curr["max"])
+    mean = format_price(insights_curr["mean"])
 
     curr_month = get_month_name(report_current.start.month)
     prev_month = get_month_name(report_previous.start.month)
 
     tax = os.getenv("TAX")
 
-    message =  f"Pörssisähkön hinta {curr_month}ssa, snt/kWh (alv {tax}%)\n"
+    message = f"Pörssisähkön hinta {curr_month}ssa, snt/kWh (alv {tax}%)\n"
     message += f"Alin: {min}\n"
     message += f"Ylin: {max}\n"
     message += f"Keskihinta: {mean}\n"
     message += f"Keskihinnan muutos {prev_month}hun: {format_difference(diff)} ({format_percentage(diff_percent)}%)\n"
 
-    message+= "\n#energia #sähkönhinta"
+    message += "\n#energia #sähkönhinta"
     print(len(message))
     return message
+
 
 def compile_weekly_message(report_current, report_previous):
 
     insights_curr = report_current.insights
     insights_prev = report_previous.insights
 
-    diff = insights_curr['mean'] - insights_prev['mean']
-    diff_percent = diff / insights_prev['mean'] * 100
-    
-    min = format_price(insights_curr['min'])
-    max = format_price(insights_curr['max'])
-    mean = format_price(insights_curr['mean'])
+    diff = insights_curr["mean"] - insights_prev["mean"]
+    diff_percent = diff / insights_prev["mean"] * 100
+
+    min = format_price(insights_curr["min"])
+    max = format_price(insights_curr["max"])
+    mean = format_price(insights_curr["mean"])
 
     tax = os.getenv("TAX")
 
-    message =  f"Pörssisähkön tuntihinnat tällä viikolla, snt/kWh (alv {tax}%)\n"
+    message = f"Pörssisähkön tuntihinnat tällä viikolla, snt/kWh (alv {tax}%)\n"
     message += f"Alin: {min}\n"
     message += f"Ylin: {max}\n"
     message += f"Keskihinta: {mean}\n"
     message += f"Muutos edelliseen viikkoon: {format_difference(diff)} ({format_percentage(diff_percent)}%)\n"
 
-    message+= "\n#energia #sähkö #hinta"
+    message += "\n#energia #sähkö #hinta"
     print(len(message))
     return message
-
-
 
 
 def compile_reply():
@@ -163,7 +172,7 @@ def compile_reply():
     latest = database.get_latest()
 
     tax = os.getenv("TAX")
-    img_path = latest['latest_7_path']
+    img_path = latest["latest_7_path"]
 
     message = f"Pörssisähkön 7 tuntihinnat, keskihinta {latest['latest_7_avg']} snt/kWh (alv {tax}%)"
 
